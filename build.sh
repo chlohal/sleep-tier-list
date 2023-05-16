6dbb8f558da300a44aad15cbeb68bf448177f6c5
chloe h <coleh@coleh.net>
2023-05-15 22:38:39 -0400

    Use npx for netlify compat

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for f in ./functions/*; do esbuild --bundle $f --platform=node > ${f/ts/js}; done
+for f in ./functions/*; do npx esbuild --bundle "$f" --platform=node > "${f/ts/js}"; done
